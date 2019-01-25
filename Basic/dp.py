698a8f7e7fc81d072b588ed0011540b56a7c04c3
Jianhuan Zeng <37096176+JianhuanZeng@users.noreply.github.com>
2019-01-25 16:36:50 -0500

    Update dp.py

--- Basic/dp.py
+++ Basic/dp.py
@@ -1,1 +1,8 @@
+# from https://www.topcoder.com/community/competitive-programming/tutorials/dynamic-programming-from-novice-to-advanced/
+# A DP is an algorithmic technique which is usually based on a recurrent formula and one (or some) starting states
+# A sub-solution of the problem is constructed from previously found ones. 
+# faster running time than other techniques like backtracking, brute-force etc.
 
+# 1. sub-problem: #how to find states #sub-functions
+## sum i, index k 
+## maximum cost, position
